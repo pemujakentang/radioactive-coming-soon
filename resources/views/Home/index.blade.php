1bf1bb2138afa1dbca875cfcc71b1626be77dc00
geraldimanuel <geraldimanuelwijaya@gmail.com>
2023-07-01 16:12:40 +0700

    feat: :sparkles: add navbar and testing dynamic routes

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -11,6 +11,13 @@
 
 <body class="h-screen bg-black">
     <div>
+        <div class="navbar bg-[#0E0EC0] justify-center gap-16">
+            <a class="font-taruno text-white text-xs no-underline hover:underline cursor-pointer" href="/home">HOME</a>
+            <a class="font-taruno text-white text-xs no-underline hover:underline cursor-pointer">VO CHALLENGE</a>
+            <a class="font-taruno text-white text-xs no-underline hover:underline cursor-pointer">RAC</a>
+            <a class="font-taruno text-white text-xs no-underline hover:underline cursor-pointer" href="ticket">CLOSING
+                NIGHT</a>
+        </div>
         <div>
             <p>Put your code here. . .</p>
         </div>
@@ -18,87 +25,69 @@
             <h2 class="font-taruno2 text-white text-center my-12">UPCOMING EVENTS</h2>
             <div class="carousel carousel-center">
                 <div class="carousel-item ml-32 border border-white">
-                    <div
-                    class="flex bg-[#0E0EC0] flex-row max-h-96">
-                    <div class="flex flex-col justify-start p-6 w-56">
-                        <h1
-                        class="mb-2 text-4xl font-taruno font-medium text-white pt-4">
-                        <b>18-30</b>
-                        </h1>
-                        <h3
-                        class="mb-2 text-3xl font-taruno font-medium text-white">
-                        <b>Sept</b>
-                        </h3>
-                    </div>
-                        <div class="relative">
-                            <img
-                                class="h-96 w-auto object-cover"
-                                src="/images/FOTO VO CHALLENGE.png"
-                                alt="" />
-                                <div class="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gradient-to-t from-black">
-                                    <div class="px-4">
-                                        <h3 class="text-3xl font-taruno text-white font-bold">
-                                            Voiceover</h3>
-                                        <h3 class="text-3xl font-taruno text-white font-bold">
-                                            Challenge</h3>
-                                    </div>
-                                </div>
+                    <div class="flex bg-[#0E0EC0] flex-row max-h-96">
+                        <div class="flex flex-col justify-start p-6 w-56">
+                            <h1 class="mb-2 text-4xl font-taruno font-medium text-white pt-4">
+                                <b>18-30</b>
+                            </h1>
+                            <h3 class="mb-2 text-3xl font-taruno font-medium text-white">
+                                <b>Sept</b>
+                            </h3>
                         </div>
-                    </div>
-                </div> 
-                <div class="carousel-item border border-white ml-16">
-                    <div
-                    class="flex bg-[#0E0EC0] max-h-96 flex-row">
-                    <div class="flex flex-col justify-start p-6 w-56">
-                        <h1
-                        class="mb-2 text-4xl font-taruno font-medium text-white pt-4">
-                        <b>28</b>
-                        </h1>
-                        <h3
-                        class="mb-2 text-3xl font-taruno font-medium text-white">
-                        <b>Oct</b>
-                        </h3>
-                    </div>
                         <div class="relative">
-                            <img
-                                class="h-96 w-auto object-cover"
-                                src="/images/FOTO RAC.png"
-                                alt="" />
-                                <div class="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gradient-to-t from-black">
-                                    <div class="px-4">
-                                        <h3 class="text-3xl font-taruno text-white font-bold">
-                                            Announcing</h3>
-                                        <h3 class="text-3xl font-taruno text-white font-bold">
-                                            Competition</h3>
+                            <img class="h-96 w-auto object-cover" src="/images/FOTO VO CHALLENGE.png" alt="" />
+                            <div class="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gradient-to-t from-black">
+                                <div class="px-4">
+                                    <h3 class="text-3xl font-taruno text-white font-bold">
+                                        Voiceover</h3>
+                                    <h3 class="text-3xl font-taruno text-white font-bold">
+                                        Challenge</h3>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div> 
-                <div class="carousel-item border border-white ml-16 mr-32">
-                    <div
-                    class="flex bg-[#0E0EC0] max-h-96">
-                    <div class="flex flex-col justify-start p-6 w-56">
-                        <h1
-                        class="mb-2 text-4xl font-taruno font-medium text-white pt-4">
-                        <b>4</b>
-                        </h1>
-                        <h3
-                        class="mb-2 text-3xl font-taruno font-medium text-white">
-                        <b>Nov</b>
-                        </h3>
-                    </div>
+                </div>
+                <div class="carousel-item border border-white ml-16">
+                    <div class="flex bg-[#0E0EC0] max-h-96 flex-row">
+                        <div class="flex flex-col justify-start p-6 w-56">
+                            <h1 class="mb-2 text-4xl font-taruno font-medium text-white pt-4">
+                                <b>28</b>
+                            </h1>
+                            <h3 class="mb-2 text-3xl font-taruno font-medium text-white">
+                                <b>Oct</b>
+                            </h3>
+                        </div>
                         <div class="relative">
-                            <img
-                                class="h-96 w-auto object-cover"
-                                src="/images/FOTO CLOSING NIGHT.png"
-                                alt="" />
-                                <div class="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gradient-to-t from-black">
-                                    <div class="px-4">
-                                        <h3 class="text-3xl font-taruno text-white font-bold">
-                                            Closing</h3>
-                                        <h3 class="text-3xl font-taruno text-white font-bold">
-                                            Night</h3>
+                            <img class="h-96 w-auto object-cover" src="/images/FOTO RAC.png" alt="" />
+                            <div class="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gradient-to-t from-black">
+                                <div class="px-4">
+                                    <h3 class="text-3xl font-taruno text-white font-bold">
+                                        Announcing</h3>
+                                    <h3 class="text-3xl font-taruno text-white font-bold">
+                                        Competition</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item border border-white ml-16 mr-32">
+                    <div class="flex bg-[#0E0EC0] max-h-96">
+                        <div class="flex flex-col justify-start p-6 w-56">
+                            <h1 class="mb-2 text-4xl font-taruno font-medium text-white pt-4">
+                                <b>4</b>
+                            </h1>
+                            <h3 class="mb-2 text-3xl font-taruno font-medium text-white">
+                                <b>Nov</b>
+                            </h3>
+                        </div>
+                        <div class="relative">
+                            <img class="h-96 w-auto object-cover" src="/images/FOTO CLOSING NIGHT.png" alt="" />
+                            <div class="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gradient-to-t from-black">
+                                <div class="px-4">
+                                    <h3 class="text-3xl font-taruno text-white font-bold">
+                                        Closing</h3>
+                                    <h3 class="text-3xl font-taruno text-white font-bold">
+                                        Night</h3>
                                 </div>
                             </div>
                         </div>
