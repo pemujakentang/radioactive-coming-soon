a7a4d9e6cedb3bd64626d37678e916277112d6df
Kesya Febriana Manampiring <kesya.febriana@student.umn.ac.id>
2023-07-05 14:12:24 +0700

    fix: :lipstick:

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -7,28 +7,17 @@
         @vite('resources/css/app.css')
         <title>UMN Radioactive 2023</title>
     </head>
-    <body>
-        <header>
-            <ul>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">VO CHALLENGE</a></li>
-                <li><a href="#">RAC</a></li>
-                <li><a href="#">CLOSING NIGHT</a></li>
-            </ul>
-        </header>
-        <div class="sec_1">
-                <li><p>THE MOST SPECTACULAR ANNUAL EVENT BY </p></li>
-                <li><img class="logo_radio" src="img/LOGO UMN RADIO.png"></li>
+
+    <body class="h-full bg-black overflow-hidden">
+        <div class="flex flex-row items-center justify-center gap-4">
+            <p class="font-taruno text-xs text-white z-10">THE MOST SPECTACULAR ANNUAL EVENT BY </p>
+            <img class="w-24 z-10" src="img/LOGO UMN RADIO.png">
         </div>
-        <center>
-            <img class="logo logo_stack" src="img/logo.gif" alt="logo"/>
-            <img class="background_logo" src="img/BACKGROUND LOGO.png" alt="background logo"/>
-        </center>
-        <script type="text/javascript">
-            window.addEventListener("scroll", function(){
-                var header = document.querySelector("header");
-                header.classList.toggle("sticky", window.scrollY > 0);
-            });
-        </script>
+        <div class="flex justify-center -m-32">
+            <img class="w-3/6 object-cover" src="img/BACKGROUND LOGO.png" alt="background logo"/>
+        </div>
+        <div class="flex justify-center absolute bottom-0 left-0 right-0">
+            <img class="w-3/6" src="img/logo.gif" alt="logo"/>
+        </div>
     </body>
 </html>
