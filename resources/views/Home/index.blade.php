8db56ea9ec2b7f0456e9a974155d69cb29d5b893
geraldimanuel <geraldimanuelwijaya@gmail.com>
2023-07-05 13:56:36 +0700

    fix: :bug: semangat vega

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -1,14 +1,28 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        @vite('resources/css/app.css')
-        <title>UMN Radioactive 2023</title>
-    </head>
-    <body>
-        <header>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>UMN Radioactive 2023</title>
+</head>
+
+<body class="bg-black h-screen">
+    <div class="navbar bg-[#0E0EC0] justify-center gap-16">
+        <a class="font-taruno text-white text-xs no-underline hover:underline cursor-pointer" href="/home">HOME</a>
+        <a class="font-taruno text-white text-xs no-underline hover:underline cursor-pointer">VO CHALLENGE</a>
+        <a class="font-taruno text-white text-xs no-underline hover:underline cursor-pointer">RAC</a>
+        <a class="font-taruno text-white text-xs no-underline hover:underline cursor-pointer" href="ticket">CLOSING
+            NIGHT</a>\
+
+    </div>
+    <div>
+        <p>Halooo</p>
+    </div>
+
+    {{-- <header>
             <ul>
                 <li><a href="#">HOME</a></li>
                 <li><a href="#">VO CHALLENGE</a></li>
@@ -23,12 +37,13 @@
         <center>
             <img class="logo logo_stack" src="img/logo.gif" alt="logo"/>
             <img class="background_logo" src="img/BACKGROUND LOGO.png" alt="background logo"/>
-        </center>
-        <script type="text/javascript">
+        </center> --}}
+    {{-- <script type="text/javascript">
             window.addEventListener("scroll", function(){
                 var header = document.querySelector("header");
                 header.classList.toggle("sticky", window.scrollY > 0);
             });
-        </script>
-    </body>
+        </script> --}}
+</body>
+
 </html>
