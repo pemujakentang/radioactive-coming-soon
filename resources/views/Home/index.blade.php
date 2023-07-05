a5985916c04c155b5f04349b03daef63169889b2
nasbi24 <nasbi24@gmail.com>
2023-07-05 13:45:39 +0700

    Update index.blade.php
    section 3 "about us"

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -11,8 +11,19 @@
 
 <body class="h-screen bg-black">
     <div>
-        <div>
-            <p>Put your code here. . .</p>
+        <div id="section2" class="my-16">
+            <h2 class="font-taruno2 text-white text-center my-12">ABOUT US</h2>
+            <p class="font-pathway text-sm text-white text-center">RADIOACTIVE merupakan acara off air tahunan yang diselenggarakan oleh UMN Radio,</p>
+            <p class="font-pathway text-sm text-white text-center">radio komunitas dari Universitas Multimedia Nusantara.</p>
+            <br>
+            <p class="font-pathway text-sm text-white text-center">Dimulai sejak 2015, RADIOACTIVE tahun in menjadi kali ke-9 acara ini terselenggara. Hal tersebut menandakan bahwa RADIOACTIVE</p>
+            <p class="font-pathway text-sm text-white text-center">berkembang dengan baik. Lebih lanjut, RADIOACTIVE diadakan sebagai brand activation untuk terus menjaga eksistensi dari UMN Radio.</p>
+            <br>
+            <p class="font-pathway text-sm text-white text-center">Pada tahun ini, acara kami mengangkat tema "RADIOACTIVE 2023: Revolution" dengan tagline "New Revolution Starts with You", yang ber-</p>
+            <p class="font-pathway text-sm text-white text-center">fokus pada pengembangan diri individu dan mendorong mereka untuk berani bertindak demi suatu perubahan dalam diri. Acara kami</p>
+            <p class="font-pathway text-sm text-white text-center">sebelumnya mengangkat tema "RADIOACTIVE 2022: Radiant" dengan tagline "Light Up Your Action” yang berfokus pada tindakan anak-anak</p>
+            <p class="font-pathway text-sm text-white text-center">muda untuk bersinar dan memancarkan cahaya positif dari diri mereka kepada orang lain di sekitarnya.</p>
+            <br>
         </div>
         <div id="section3" class="my-16">
             <h2 class="font-taruno2 text-white text-center my-12">UPCOMING EVENTS</h2>
