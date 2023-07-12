88f28e80f0e9d5c62d10f41f61efdb083c35ee81
marcelj33 <marcel.jonathan@student.umn.ac.id>
2023-07-12 19:00:31 +0700

    Footer
    a bit trouble di svg

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -11,15 +11,14 @@
 
 <body class="h-screen bg-black">
     <div>
-        <div>
-            <p>Put your code here. . .</p>
+        <div id="section4" class="my-16">
+            <p>Halo...</p>
         </div>
         <div id="section3" class="my-16">
             <h2 class="font-taruno2 text-white text-center my-12">UPCOMING EVENTS</h2>
             <div class="carousel carousel-center">
                 <div class="carousel-item ml-32 border border-white">
-                    <div
-                    class="flex bg-[#0E0EC0] flex-row max-h-96">
+                    <div class="flex bg-[#0E0EC0] flex-row max-h-96">
                     <div class="flex flex-col justify-start p-6 w-56">
                         <h1
                         class="mb-2 text-4xl font-taruno font-medium text-white pt-4">
@@ -109,6 +108,53 @@
         <div>
             <p>Put your code here. . .</p>
         </div>
+        <footer class="bg-black py-6">
+        <div class="flex flex-row">
+            <div class="container w-full ml-5">
+                    <img src="/images/LOGO RA.png" alt="" class="h-40">
+            </div>
+            <div class="container flex flex-col justify-evenly">
+                <h5 class="text-xs font-taruno font-medium text-white">
+                    <b>HOME</b>
+                </h5>
+                <h5 class="text-xs font-taruno font-medium text-white">
+                    <b>VO CHALLENGE</b>
+                </h5>
+                <h5 class="text-xs font-taruno font-medium text-white">
+                    <b>RAC</b>
+                </h5>
+                <h5 class="text-xs font-taruno font-medium text-white">
+                    <b>CLOSING NIGHT</b>
+                </h5>
+            </div>
+            <div class="mx-auto flex flex-wrap content-center">
+                <div class="flex space-x-4 mt-4 sm:mt-0">
+                <a target="_blank" rel="noopener" data-testid="SocialIcon" href="mailto:mediarelation.radioactive@gmail.com" aria-label="RADIOACTIVE 2023 email address" class="sc-eCssSg lbrsth sc-fodVxV cYLuAZ"><svg enable-background="new 0 0 24 24" viewBox="0 0 24 24" class="sc-gKsewC cVBMqs"><title data-testid="svgTitle" id="title_0.4142702403195657">email</title><path d="M18.821,20.5H5.179A3.683,3.683,0,0,1,1.5,16.821V7.179A3.683,3.683,0,0,1,5.179,3.5H18.821A3.683,3.683,0,0,1,22.5,7.179v9.642A3.683,3.683,0,0,1,18.821,20.5ZM5.179,4.5A2.682,2.682,0,0,0,2.5,7.179v9.642A2.682,2.682,0,0,0,5.179,19.5H18.821A2.682,2.682,0,0,0,21.5,16.821V7.179A2.682,2.682,0,0,0,18.821,4.5Z"></path><path d="M12,14.209a.5.5,0,0,1-.346-.138L4.286,7.028a.5.5,0,0,1,.691-.723L12,13.018l7.023-6.713a.5.5,0,1,1,.691.723l-7.368,7.043A.5.5,0,0,1,12,14.209Z"></path><path d="M4.7,17.833a.5.5,0,0,1-.347-.86l5.54-5.31a.5.5,0,0,1,.692.722L5.048,17.694A.5.5,0,0,1,4.7,17.833Z"></path><path d="M19.3,17.832a.5.5,0,0,1-.346-.139l-5.538-5.308a.5.5,0,0,1,.692-.722l5.538,5.308a.5.5,0,0,1-.346.861Z"></path>
+                    </svg>
+                </a>
+                    <a href="https://example.com/twitter" target="_blank" class="text-white">
+                        <svg class="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M23 4.996c-.884.392-1.832.656-2.832.776 1.016-.6 1.8-1.556 2.168-2.688-.956.56-2.016.968-3.152 1.192-.9-.964-2.184-1.56-3.6-1.56-2.72 0-4.92 2.2-4.92 4.92 0 .392.04.776.112 1.144C7.064 8.7 4.024 7.2 1.656 4.68.376 6.04.4 7.648.992 8.952c-.896-.088-1.736-.28-2.48-.56v.064c0 1.6 1.16 2.928 2.688 3.232-.28.08-.56.128-.864.128-.216 0-.424-.024-.624-.072.424 1.312 1.64 2.28 3.08 2.312-1.128.88-2.552 1.4-4.104 1.4-.28 0-.56-.024-.84-.072 1.48.96 3.232 1.528 5.12 1.528 6.12 0 9.496-5.08 9.496-9.496 0-.144 0-.288-.008-.432C21.12 6.92 22.12 5.496 23 4.996z"/>
+                        </svg>
+                    </a>
+                    <a href="https://example.com/instagram" target="_blank" class="text-white">
+                        <svg class="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8zm4.309-14.254a1.313 1.313 0 0 0-1.856-.045 1.32 1.32 0 0 0-.044 1.864c.62.615.903 1.432.835 2.25h1.881c.013-.18.033-.355.033-.535a3.317 3.317 0 0 0-6.633-.001v.535H9.04v-.535a3.319 3.319 0 0 0-6.638 0c0 .18.02.355.033.535h1.884a3.294 3.294 0 0 0 .835-2.25c-.068-.817.215-1.634.835-2.25a1.32 1.32 0 0 0-1.856-1.864A5.263 5.263 0 0 0 7.69 9.972v1.784a5.275 5.275 0 0 0 5.272 5.272h.001A5.275 5.275 0 0 0 18.21 11.756V9.972a5.263 5.263 0 0 0-1.562-3.776z"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="flex justify-center items-center content-center w-full p-3">
+                    <img src="/images/LOGO UMN RADIO.png" alt="" class="h-20">
+                </div>
+            <div class="flex justify-center w-full p-3">
+                    <img src="/images/LOGO UMN.png" alt="" class="h-40">
+            </div>
+        </div>
+        <div class="container mx-auto mt-1 text-center">
+                <p class="mb-2 text-xs font-taruno font-medium text-white">&copy; UMN RADIOACTIVE 2023</p>
+        </div>
+        </footer>
     </div>
 </body>
 
